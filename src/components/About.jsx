e97ebdb7a8ecd5877db19e696bd54bfd615a6969
Abdurrub123 <112121325+Abdurrub123@users.noreply.github.com>
2024-12-13 05:43:10 +0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-        
+    <div className='w-full py-20 bg-[#CDEA68] rounded-tl-[14px] rounded-tr-[14px]'>
+        <div className="about-text">
+          <h1>Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
+        </div>
     </div>
   )
 }
